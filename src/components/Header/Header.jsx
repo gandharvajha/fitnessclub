319ef9bdda3c5412017a20b5607bb7ca270a5804
gandharvajha <gandharva1371998@gmail.com>
2024-05-01 18:49:53 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+import Logo from "../../assets/logo.png";
+
+const Header= ()=>{
+    return(
+        <div>
+            <div className="header">
+                <img src={Logo} alt="" className="logo"/>
+                <ul className="header-menu">
+                    <li>Home</li>
+                    <li>Program</li>
+                    <li>Why us</li>
+                    <li>Plans</li>
+                    <li>Testimonials</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
